ff70722c7980dd6821b07dca00d4c56815b6e075
miruna suliman <miruna.suliman@yahoo.com>
2023-05-06 20:18:56 +0300

    Add project files.

---
+++ Barcode/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Barcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            p = new PictureBox();
+            valoare = new TextBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            splitter1 = new Splitter();
+            val_fin = new TextBox();
+            button1 = new Button();
+            panel1 = new Panel();
+            label1 = new Label();
+            panel2 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)p).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // p
+            // 
+            p.BackColor = Color.White;
+            p.BorderStyle = BorderStyle.Fixed3D;
+            p.Location = new Point(147, 17);
+            p.Name = "p";
+            p.Size = new Size(349, 116);
+            p.TabIndex = 0;
+            p.TabStop = false;
+            // 
+            // valoare
+            // 
+            valoare.Location = new Point(127, 127);
+            valoare.Name = "valoare";
+            valoare.Size = new Size(208, 27);
+            valoare.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(18, 21);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(149, 24);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "deseneaza chenar";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(18, 80);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(178, 24);
+            checkBox2.TabIndex = 3;
+            checkBox2.Text = "negativ (alb pe negru)";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(18, 50);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(183, 24);
+            checkBox3.TabIndex = 4;
+            checkBox3.Text = "deseneaza valoare text";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // splitter1
+            // 
+            splitter1.Location = new Point(0, 0);
+            splitter1.Name = "splitter1";
+            splitter1.Size = new Size(4, 429);
+            splitter1.TabIndex = 5;
+            splitter1.TabStop = false;
+            // 
+            // val_fin
+            // 
+            val_fin.BorderStyle = BorderStyle.None;
+            val_fin.Font = new Font("SimSun", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            val_fin.Location = new Point(160, 111);
+            val_fin.Name = "val_fin";
+            val_fin.Size = new Size(324, 23);
+            val_fin.TabIndex = 6;
+            val_fin.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.System;
+            button1.Location = new Point(228, 319);
+            button1.Name = "button1";
+            button1.Size = new Size(167, 44);
+            button1.TabIndex = 7;
+            button1.Text = "Genereaza";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(checkBox1);
+            panel1.Controls.Add(checkBox3);
+            panel1.Controls.Add(checkBox2);
+            panel1.Controls.Add(valoare);
+            panel1.Location = new Point(172, 152);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(349, 174);
+            panel1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Cursor = Cursors.IBeam;
+            label1.Font = new Font("SimSun", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(4, 123);
+            label1.Name = "label1";
+            label1.Size = new Size(117, 28);
+            label1.TabIndex = 5;
+            label1.Text = "Valoare";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Pink;
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(val_fin);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(p);
+            panel2.Location = new Point(23, 11);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(669, 394);
+            panel2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightPink;
+            ClientSize = new Size(711, 429);
+            Controls.Add(panel1);
+            Controls.Add(splitter1);
+            Controls.Add(panel2);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)p).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox p;
+        private System.Windows.Forms.TextBox valoare;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.TextBox val_fin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Barcode/Form1.cs
@@ -0,0 +1,138 @@
+﻿namespace Barcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void deseneaza(Graphics g)
+        {
+
+
+            Point a = new Point(p.Size.Width / 4, p.Size.Height / 2 - 50);
+            Point b = new Point(p.Size.Width / 4, p.Size.Height / 2 + 50);
+            //Am setat coordonatele punctelor ce determina prima linie
+
+
+            FileStream fs = null; StreamReader sr = null;
+            fs = new FileStream("cod.txt", FileMode.Open);
+            sr = new StreamReader(fs);
+            string cod = sr.ReadToEnd();
+            sr.Close();
+            fs.Close();
+            //Am citit din fisier tabelul si l-am copiat in string-ul cod
+
+
+            int[] lg = new int[50];
+            lg['0'] = 3;
+            lg['1'] = 5;
+            //Am determinat lungimea liniilor
+
+            Color c;
+            int i, l = 0, nr = 1;
+            int si = 0, sp = 0;
+            int val, kk, key = 0;
+
+            string v0 = valoare.Text; //valoarea intiala citita din textbox
+
+            for (i = 0; i < v0.Length; i++)
+            {
+                val = Convert.ToInt32(v0[i]) - 48;
+                if (i % 2 == 0)
+                    si = si + val;
+                else sp = sp + val;
+
+            }
+            kk = si + sp * 3;
+            while (kk % 10 != 0)
+            {
+                key++;
+                kk++;
+            }
+            string v = ("0" + v0 + key);
+            val_fin.Text = v;
+            val_fin.Visible = false;
+            //Calculam valoarea finala
+
+            if (v.Length % 2 == 0)
+            {
+                for (i = 0; i < v.Length; i++)
+                {
+                    int ok = 0;
+                    for (int j = 0; j < 60; j = j + 6)
+                    {
+
+                        if (v[i].CompareTo(cod[j]) == 0)
+                        {
+
+                            a.Offset((l + lg[cod[j + 1]]) / 2, 0);
+                            b.Offset((l + lg[cod[j + 1]]) / 2, 0);
+
+
+                            ok = 1;
+                            for (int k = j + 1; k < j + 6; k++)
+                            {
+
+                                if (checkBox2.Checked == false)
+                                {
+                                    if (nr % 2 == 1)
+                                        c = Color.Black;
+                                    else c = Color.White;
+
+
+                                }
+                                else
+                                {
+                                    if (nr % 2 == 0)
+                                        c = Color.Black;
+                                    else c = Color.White;
+
+                                }
+
+                                nr++;
+                                g.DrawLine(new Pen(c, lg[cod[k]]), a, b);
+
+
+                                if (k < j + 5)
+                                {
+                                    a.Offset((lg[cod[k]] + lg[cod[k + 1]]) / 2, 0);
+                                    b.Offset((lg[cod[k]] + lg[cod[k + 1]]) / 2, 0);
+
+                                }
+                                l = lg[cod[j + 5]];
+
+                            }
+
+                        }
+                        if (ok == 1) break;
+                    }
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Valorile acceptate trebuie să aibă un număr par de cifre!");
+                val_fin.Text = null;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+                p.BackColor = Color.Black;
+            else p.BackColor = Color.White;
+
+            if (checkBox1.Checked == true)
+                deseneaza(p.CreateGraphics());
+            else p.CreateGraphics().Clear(p.BackColor);
+
+            if (checkBox3.Checked == true)
+                val_fin.Visible = true;
+
+        }
+
+    }
+}
